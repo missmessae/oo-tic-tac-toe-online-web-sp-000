16b9debdd6593da7de1c562ce9a7d8e6a2032f0b
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 22:58:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,12 +16,12 @@ def initialize
 end
 
 #HELPERS
-def display_board(@board)
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 
@@ -30,19 +30,19 @@ def input_to_index(input)
 end
 
 
-def position_taken?(@board, index)
-  if @board[index] == " " || @board[index] == "" || @board[index] == nil
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif @board[index] == "X" || @board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
 
 
-def valid_move?(@board, index)
-  if position_taken?(@board, index) == false && index.between?(0, 8)
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
     return true
-  elsif position_taken?(@board, index)
+  elsif position_taken?(board, index)
     return false
   elsif index < 0 || index > 8
     return false
