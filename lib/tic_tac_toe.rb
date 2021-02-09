24789108dfdd6f67eef360f13b65345128690d84
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 23:02:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,24 +30,12 @@ def input_to_index(input)
 end
 
 
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  elsif board[index] == "X" || board[index] == "O"
-    return true
-  end
+def move(board, index, character)
+    board[index] = character
 end
 
 
-def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0, 8)
-    return true
-  elsif position_taken?(board, index)
-    return false
-  elsif index < 0 || index > 8
-    return false
-  end
-end
+
 
 
 end
