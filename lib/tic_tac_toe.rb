9b6d160d0f84e0f9301463fb4fd8752c42993c60
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 23:00:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,7 @@ def initialize
 end
 
 #HELPERS
-def display_board(board)
+def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
