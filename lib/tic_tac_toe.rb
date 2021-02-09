28bb71a937d926fa0a5360aa464f1460fc7db1c0
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 23:14:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,6 +37,25 @@ def move(index, character)
 end
 
 
+def position_taken?(index)
+  if @board[index] == " " || @board[index] == "" || @board[index] == nil
+    return false
+  elsif @board[index] == "X" || @board[index] == "O"
+    return true
+  end
+end
+
+
+def valid_move?(index)
+  if position_taken?(@board, index) == false && index.between?(0, 8)
+    return true
+  elsif position_taken?(@board, index)
+    return false
+  elsif index < 0 || index > 8
+    return false
+  end
+end
+
 
 
 
